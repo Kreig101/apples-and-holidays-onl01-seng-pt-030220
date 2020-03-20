ef280a34e09cd60ae0adea47449a5945da2a3a86
Craig Storer <craigstorer@gmail.com>
2020-03-20 17:56:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -116,7 +116,9 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-# hash[:winter].map {|season, supplies| [ supplies,"wings"] }.flatten
+      
+    holiday_hash.map do |season, |
+      
 end
 
 
